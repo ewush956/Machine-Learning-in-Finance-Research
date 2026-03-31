b5a050daf258afbbaf9e3f6be3e094327f16b20f
Dru <91165792+awals829@users.noreply.github.com>
2026-03-31 13:35:59 -0600

    added id for colour theme mode.

--- app/ui.py
+++ app/ui.py
@@ -1,25 +1,39 @@
+# #########################################################
+# #                      Imports                          #
+# #########################################################
 from shiny import ui
 from Tabs.DataTable.datatable import datatable_tab_ui
 from Tabs.StandardDeviation.tab_stddev  import stddev_tab_ui
 from Tabs.SharpeRatio.tab_sharpe_ratio import sharpe_ratio_tab_ui
 from llm_panel import llm_panel_ui
+from ticker_info import company_tickers
 
 import pandas  as pd
-from ticker_info import fuggin_stonks
-
-end = pd.Timestamp.now()
-start = end - pd.DateOffset(years=1)
 
 
+# ###########################################################
+# #                      Constants                          #
+# ###########################################################
+END_DATE = pd.Timestamp.now()
+START_DATE = END_DATE - pd.DateOffset(years=1)
+
+
+# ################################################################
+# #                      File Functions                          #
+# ################################################################
 def _load_ticker_choices() -> dict[str, str]:
     choices = {}
-    for company, ticker in fuggin_stonks.items():
+    for company, ticker in company_tickers.items():
         choices[ticker] = f"{company} ({ticker})"
     return dict(sorted(choices.items(), key=lambda item: item[1]))
 
+
+# #####################################################################
+# #                      Application UI Main                          #
+# #####################################################################
 app_ui = ui.page_sidebar(
     ui.sidebar(
-        ui.input_dark_mode(mode='dark'),
+        ui.input_dark_mode(id="color_mode", mode='dark'),
         ui.h4("Finance Metrics Dashboard", align=""),
         ui.hr(),
         ui.p("Yahoo Finance Ticker Search"),
@@ -87,7 +101,7 @@ app_ui = ui.page_sidebar(
             });
             """
         ),
-        ui.input_date_range("dates", "Select dates", start=start, end=end),
+        ui.input_date_range("dates", "Select dates", start=START_DATE, end=END_DATE),
         llm_panel_ui(),
         width="20rem"
     ),
