960771a8780555b4946cc2b9d9135d19b9c68daf
Dru <91165792+awals829@users.noreply.github.com>
2026-04-01 09:14:59 -0600

    Adjusted Paths for imports with init files and new path.py master file.

--- app/Modules/helpers.py
+++ app/Modules/helpers.py
@@ -1,13 +1,12 @@
 from shiny import ui
 from pathlib import Path
-
-APP_DIR = Path(__file__).resolve().parent.parent
+from paths import MARKDOWN_DIR
 
 ''' ========== Global Helper Functions ========== '''
 def load_html(filepath: str):
     p = Path(filepath)
     if not p.is_absolute():
-        p = (APP_DIR / p).resolve()
+        p = (MARKDOWN_DIR / p).resolve()
     try:
         return ui.HTML(p.read_text(encoding="utf-8"))
     except FileNotFoundError:
